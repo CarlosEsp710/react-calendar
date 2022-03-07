8b5a2efb5420d85131995b55b4e317c04774edd3
Carlos Espejel <carlosespejel71@gmail.com>
2022-03-07 12:45:45 -0600

    Configuración del calendario

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import "moment/locale/es-mx";
 
 import { Navbar } from "../ui/Navbar";
+import { messages } from "../../helpers/calendar-messages-es";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
+moment.locale("es");
 const localizer = momentLocalizer(moment);
 
 const events = [
@@ -19,6 +22,19 @@ const events = [
 ];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      color: "white",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+    };
+    return {
+      style,
+    };
+  };
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -27,6 +43,8 @@ export const CalendarScreen = () => {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   );
