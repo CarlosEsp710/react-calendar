2aa9fa59d8806167079ae7f8b5058c24f721dc70
Carlos Espejel <carlosespejel71@gmail.com>
2022-03-07 12:34:27 -0600

    Diseño calendario

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 
 import { CalendarApp } from "./CalendarApp";
 
+import "../src/assets/styles/calendar.css";
+import "../src/assets/styles/styles.css";
+
 ReactDOM.render(<CalendarApp />, document.getElementById("root"));
