8e56f59a725ffd1dcade935613f5f7f810a67a85
Carlos Espejel <carlosespejel71@gmail.com>
2022-03-07 13:14:31 -0600

    Eventos del calendario

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "moment/locale/es-mx";
 
+import { CalendarEvent } from "./CalendarEvent";
 import { Navbar } from "../ui/Navbar";
 import { messages } from "../../helpers/calendar-messages-es";
 
@@ -18,10 +19,31 @@ const events = [
     start: moment().toDate(),
     end: moment().add(2, "hours").toDate(),
     bgcolor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Carlos",
+    },
   },
 ];
 
 export const CalendarScreen = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "month"
+  );
+
+  const onDoubleClick = (event) => {
+    console.log(event);
+  };
+
+  const onSelectEvent = (event) => {
+    console.log(event);
+  };
+
+  const onViewChangeEvent = (event) => {
+    setLastView(event);
+    localStorage.setItem("lastView", event);
+  };
+
   const eventStyleGetter = (event, start, end, isSelected) => {
     const style = {
       backgroundColor: "#367CF7",
@@ -45,6 +67,13 @@ export const CalendarScreen = () => {
         endAccessor="end"
         messages={messages}
         eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onView={onViewChangeEvent}
+        onSelectEvent={onSelectEvent}
+        view={lastView}
+        components={{
+          event: CalendarEvent,
+        }}
       />
     </div>
   );
